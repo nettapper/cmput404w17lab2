f866f6d03b1228b2f0600f3678240048cb3ff908
nettapper <connerdunn7399@gmail.com>
2017-01-17 09:27:22 -0700

    the proxy file mostly works.... not showing any port in use error

---
+++ proxy.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import socket
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # should help the 'port in use'
+
+# Note: 0.0.0.0 will listen on ALL addrs for this machine
+serverSocket.bind(("0.0.0.0", 8111))  # only root users can use ports less than 1024
+serverSocket.listen(5)  # start listening, up to 5 connections in a queue
+
+while True:
+  (incomingSocket, address) = serverSocket.accept()
+  print "We got a connection from %s" % (str(address))
+
+  clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+  clientSocket.connect(("www.google.com", 80))
+
+  incomingSocket.setblocking(0)
+  clientSocket.setblocking(0)
+
+  while True:  # keep checking if client/server has something to say
+    request = bytearray()
+    while True:  # request connection from client
+      try:
+        part = incomingSocket.recv(1024)
+      except IOError, e:
+        if e.errno == 11:
+          part = None
+        else:
+          raise
+      if (part):
+        clientSocket.sendall(part)
+        request.extend(part)
+      else:
+        break
+
+    if len(request) > 0:
+      print "### REQUEST ###"
+      print request
+
+    response = bytearray()
+    while True:  # response from server
+      try:
+        part = clientSocket.recv(1024)
+      except IOError, e:
+        if e.errno == 11:
+          part = None
+        else:
+          raise
+      if (part):
+        incomingSocket.sendall(part)
+        response.extend(part)
+      else:
+        break
+
+    if len(response) > 0:
+      print "### RESPONSE ###"
+      print response
